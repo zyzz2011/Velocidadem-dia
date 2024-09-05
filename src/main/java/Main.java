1cc2477f5bb456948bd8eaff3f301f24c9b703b5
zyzz2011 <169068710+zyzz2011@users.noreply.github.com>
2024-09-05 10:46:08 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("calcular a velocidade media em km/h");
+
+System.out.println("");
+
+System.out.println("a distancia considerado é:");
+
+    double distancia = 400;
+
+    System.out.println(distancia);
+
+    System.out.println("o tempo variavel é:");
+
+double tempo = 3;
+
+  System.out.println(tempo);
+
+double velocidadeMedia = distancia / tempo;
+    System.out.println("velocidadeMedia é : 133,33334");
+  }
+}
